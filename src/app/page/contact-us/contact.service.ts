742f2cdd002e02aad26086bb60bbee4b0add8cae
trueOpatcher <maxx.tiechko@gmail.com>
2022-06-24 00:32:57 +0300

    fix

--- src/app/page/contact-us/contact.service.ts
+++ src/app/page/contact-us/contact.service.ts
@@ -19,7 +19,7 @@ export class ContactService {
         formData.append('companyName', info.companyName);
         formData.append('number', info.number);
         formData.append('email', info.email);
-        formData.append('descs', info.desc);
+        formData.append('desc', info.desc);
 
 
        return this.http.post('https://message-endpoint/contact/send', formData).pipe(take(1));
